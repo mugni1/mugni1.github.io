2a1ecbe0acd8464afff73edabe272715d9bfe53e
malascoding <asepabdulmugni22@gmail.com>
2024-08-06 11:52:56 +0700

    modified index,script,input,style and responsive navbar

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-//hambureger
+///hambureger
 const hambureger = document.querySelector("#hamburger");
 const garis = hambureger.querySelectorAll("div");
 
 hambureger.addEventListener("click", function () {
   menu_item.classList.toggle("hidden");
+  garis[0].classList.toggle("h1");
+  garis[1].classList.toggle("hidden");
+  garis[2].classList.toggle("h3");
 });
 
 //navbar fixed
@@ -19,25 +22,29 @@ window.onscroll = function () {
     nav.classList.add("bg-white");
     brand.classList.remove("text-white");
     menu_item.classList.remove("text-white");
+    menu_item.classList.remove("bg-emerald-500");
+    menu_item.classList.add("bg-white");
     contac_us.classList.add("bg-emerald-500");
     contac_us.classList.add("text-white");
     // contac_us.classList.remove("text-black");
 
     //hamburger
-    garis[0].classList.add("bg-black");
-    garis[1].classList.add("bg-black");
-    garis[2].classList.add("bg-black");
+    garis[0].classList.add("black");
+    garis[1].classList.add("black");
+    garis[2].classList.add("black");
     // back_top.classList.add("show");
   } else {
     nav.classList.remove("bg-white");
     brand.classList.add("text-white");
     menu_item.classList.add("text-white");
+    menu_item.classList.add("bg-emerald-600");
+    menu_item.classList.remove("bg-white");
     contac_us.classList.remove("bg-emerald-500");
     contac_us.classList.remove("text-white");
 
-    garis[0].classList.remove("bg-black");
-    garis[1].classList.remove("bg-black");
-    garis[2].classList.remove("bg-black");
+    garis[0].classList.remove("black");
+    garis[1].classList.remove("black");
+    garis[2].classList.remove("black");
     // back_top.classList.remove("show");
   }
 };
